52137acb7cdfb4e1cd4e39ea1c39067332845749
Dingkun_Liu <91831067+Dingkun0817@users.noreply.github.com>
2025-03-25 13:12:29 +0800

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,12 +1,10 @@
 import random
 import numpy as np
 
-
 def split_data(data, axis, times):
     # Splitting data into multiple sections. data: (trials, channels, time_samples)
     data_split = np.split(data, indices_or_sections=times, axis=axis)
     return data_split
-
 
 def convert_label(labels, axis, threshold):
     # Converting labels to 0 or 1, based on a certain threshold
@@ -14,12 +12,10 @@ def convert_label(labels, axis, threshold):
     print(label_01)
     return label_01
 
-
 def time_cut(data, cut_percentage):
     # Time Cutting: cut at a certain percentage of the time. data: (..., ..., time_samples)
     data = data[:, :, :int(data.shape[2] * cut_percentage)]
     return data
-
 
 def traintest_split_cross_subject(dataset, X, y, num_subjects, test_subject_id):
     data_subjects = np.split(X, indices_or_sections=num_subjects, axis=0)
@@ -31,7 +27,6 @@ def traintest_split_cross_subject(dataset, X, y, num_subjects, test_subject_id):
     print('Test subject s' + str(test_subject_id))
     print('Training/Test split:', train_x.shape, test_x.shape)
     return train_x, train_y, test_x, test_y
-
 
 def traintest_split_domain_classifier(dataset, X, y, num_subjects, test_subject_id):
     data_subjects = np.split(X, indices_or_sections=num_subjects, axis=0)
@@ -45,4 +40,3 @@ def traintest_split_domain_classifier(dataset, X, y, num_subjects, test_subject_
     print('Test subject s' + str(test_subject_id))
     print('Training:', train_x.shape, train_y.shape)
     return train_x, train_y, None, None
-
